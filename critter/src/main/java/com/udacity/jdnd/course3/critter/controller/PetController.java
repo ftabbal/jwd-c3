5e60b1be0b1beb5475146962faa2f7a0352f7bcb
Fred <fred@home.local>
2020-12-06 22:05:42 -0500

    Group entities into a data package

--- critter/src/main/java/com/udacity/jdnd/course3/critter/controller/PetController.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/controller/PetController.java
@@ -1,6 +1,6 @@
 package com.udacity.jdnd.course3.critter.controller;
 
-import com.udacity.jdnd.course3.critter.pet.PetDTO;
+import com.udacity.jdnd.course3.critter.data.pet.PetDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
