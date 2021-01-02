a4e082d3ed834f9f39348125f60ed84d85b60313
Fred <fred@home.local>
2021-01-01 22:06:33 -0500

    Add properties that are specific to the Employee type.

--- critter/src/main/java/com/udacity/jdnd/course3/critter/data/user/Employee.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/data/user/Employee.java
@@ -1,7 +1,20 @@
 package com.udacity.jdnd.course3.critter.data.user;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.DayOfWeek;
+import java.util.Set;
 
 @Entity
 public class Employee extends User {
+
+    @ElementCollection(targetClass =  DayOfWeek.class)
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> daysAvailable;
+
+    @ElementCollection(targetClass = EmployeeSkill.class)
+    @Enumerated(EnumType.STRING)
+    private Set<EmployeeSkill> skill;
 }
